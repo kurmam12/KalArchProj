12a71d2bc1696919455dc4179e982273b5a18661
kurmam12 <kurmam1212@outlook.com>
2025-12-10 09:10:37 +0530

    this vehicleownr project
    this vehicleownr project

---
+++ VehicleOwner/src/test/java/com/cog/pract/service/VehicleOwnerServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.cog.pract.service;
+
+/*import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;*/
+
+class VehicleOwnerServiceImplTest {
+
+	/*
+	 * @Test void test() { fail("Not yet implemented"); }
+	 */
+
+}
